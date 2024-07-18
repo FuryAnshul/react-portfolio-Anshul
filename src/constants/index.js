4494b3d1a922e5a5e3878e4190e33b87de498279
FuryAnshul <anshulramdham91@gmail.com>
2024-07-18 20:37:13 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = 'I am a passionate full stack developer with a strong interest in crafting robust and scalable web applications. As a recent graduate, I have gained valuable experience through various projects and internships. I have honed my skills in front-end technologies like HTML, CSS, and JavaScript, as well as back-end technologies like Java, PHP, and MySQL. My goal is to leverage my growing expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.';
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. As a recent graduate, I have gained valuable experience through various projects and internships, working with a variety of technologies, including HTML, CSS, JavaScript, Java, PHP, and MySQL. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Web Developer",
+    company: "India Active Software Inc.",
+    description: `Led a team in developing and maintaining web applications using HTML, CSS, JavaScript, PHP, and MySQL. Implemented RESTful APIs and integrated with MySQL databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["HTML", "CSS", "Javascript", "PHP", "MySQL"],
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "",
+    technologies: [],
+  },
+  {
+    title: "Healthcare Information System",
+    image: project2,
+    description:
+      "An application for managing hospital healthcare tasks and projects, with features such as patient record management, appointment scheduling, staff assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Java", "JSP", "MySQL"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+];
+
+export const CONTACT = {
+  address: "6A Ambar Colony, Nagpur-440023 ",
+  phoneNo: "+91 8421 2773 79  ",
+  email: "ramdhamanshul@gmail.com",
+};
